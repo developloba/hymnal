60bf85c2d520db1fbf4d1050fcc296fa9d557f5f
Loba <iamtobi09@gmail.com>
2022-10-29 06:49:12 +0100

    new app commit

---
+++ lib/utils/constant.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+Color kprimarycolor = const Color.fromARGB(255, 232, 117, 121);
+Color kprimarycolorlight = const Color.fromARGB(45, 232, 117, 121);
+TextStyle ktitleblack = const TextStyle(
+  fontFamily: 'dmbold',
+  fontSize: 45,
+  color: Colors.black,
+);
+TextStyle ktitlered =
+    TextStyle(color: kprimarycolor, fontFamily: 'dm', fontSize: 30);
+TextStyle kbody =
+    const TextStyle(fontFamily: 'dm', fontSize: 25, color: Colors.black);
+
+TextStyle kbodygrey =
+    const TextStyle(fontFamily: 'dm', fontSize: 25, color: Colors.grey);
+TextStyle kbodygreysmall =
+    const TextStyle(fontFamily: 'dm', fontSize: 15, color: Colors.grey);
+TextStyle kbodysmall =
+    const TextStyle(fontFamily: 'dm', fontSize: 15, color: Colors.black);
